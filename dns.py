de32f50cdfde60fae7d0c05843b986b9a90a72dd
Thomas Sørensen <tsorensen007@gmail.com>
2024-10-25 16:17:17 +0200

    feat: add flaskto dns

---
+++ dns.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/rasberry")
+def rasberry():
+    return render_template('rasberry.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
